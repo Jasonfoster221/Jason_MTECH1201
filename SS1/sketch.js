20c2b91b99f8b1cb0468e4274b81b176985d6d2d
Jasonfoster221 <90210foster@gmail.com>
2026-09-17 14:41:48 -0400

    Update sketch.js

--- SS1/sketch.js
+++ SS1/sketch.js
@@ -1,15 +1,21 @@
 function setup() {
   createCanvas(400, 400);
-}
+}/*Jason Foster
+title: dawn pyramid
+what i am trying to d0 this semester
+is make an image move and this ties into
+the image because with time i want to make
+it look like time has past within this picture*/
 
 function draw() {
   background(100,200,100);
   fill(255,255,0);
+  strokeweight(4,255);
  circle (200,100,200);
  fill(255,225,225);
 triangle(0,400,400,400,200,100);
 line(100,100,300,100);
 fill(255,255,255);
-strokeWeight(4,255);
+strokeWeight(2,255);
 quad(50,65,85,50,50,40,15,50);
 }
