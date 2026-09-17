6d3031c74bdcc13e56ac47580f4abce97501ed14
Jasonfoster221 <90210foster@gmail.com>
2026-09-17 13:48:47 -0400

    added 1st short study files

---
+++ SS1/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100,200,100);
+  fill(255,255,0);
+ circle (200,100,200);
+ fill(255,225,225);
+triangle(0,400,400,400,200,100);
+line(100,100,300,100);
+fill(255,255,255);
+strokeWeight(4,255);
+quad(50,65,85,50,50,40,15,50);
+}
